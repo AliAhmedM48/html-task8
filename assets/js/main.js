4f0ba3859c41d74d72d7d0b8471bd41b8a30e178
Ali Ahmed <63220520+AliAhmedM48@users.noreply.github.com>
2023-11-25 22:11:16 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,39 @@
 
+// ! region js basics info
+//#region js basics info
+// * Both $(function () {}) and $(window).on("load", function () {})
+// * are jQuery constructs used to execute code when the document
+// * or all the page assets have finished loading.  
+
+/* the differences:
+######################
+$(function () {}):
+--------------------
+- This is a shorthand for $(document).ready(function () {}).
+- It runs the provided function as soon as the DOM is fully loaded.
+- It doesn't wait for external resources like images to be completely loaded.
+
 $(function () {
-    //     "use strict";
+    // Your code here
+});
+######################
+$(window).on("load", function () {}):
 
-    //     var wind = $(window);
+- This waits for the entire page, including all images
+  and other external resources, to be fully loaded.
+- It ensures that the function is executed only after all assets (images, stylesheets, etc.) have been downloaded.
 
-    //     // scrollIt
-    //     $.scrollIt({
-    //         upKey: 38, // key code to navigate to the next section
-    //         downKey: 40, // key code to navigate to the previous section
-    //         easing: "swing", // the easing function for animation
-    //         scrollTime: 600, // how long (in ms) the animation takes
-    //         activeClass: "active", // class given to the active nav element
-    //         onPageChange: null, // function(pageIndex) that is called when page is changed
-    //         topOffset: -60, // offste (in px) for fixed top navigation
-    //     });
+$(window).on("load", function () {
+    // Your code here
+});
+######################
+*/
+//#endregion
 
-    //     // close navbar-collapse when a  clicked
-    //     $(".nav a").on("click", function () {
-    //         $(".navbar-collapse").removeClass("in").addClass("collapse");
-    //     });
+$(function () {
 
-    //     // navbar scrolling background
-    //     wind.on("scroll", function () {
-    //         var bodyScroll = wind.scrollTop(),
-    //             navbar = $(".navbar-default"),
-    //             h_hight = $(".header").outerHeight();
-
-    //         if (bodyScroll > h_hight) {
-    //             navbar.addClass("nav-scroll");
-    //         } else {
-    //             navbar.removeClass("nav-scroll");
-    //         }
-    //     });
-
-    // // ^ progress bar
-    // wind.on("scroll", function () {
-    //     $(".skills .progress .progress-bar").each(function () {
-    //         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
-    //         var myVal = $(this).attr("data-value");
-    //         if (bottom_of_window > bottom_of_object) {
-    //             $(this).css({
-    //                 width: myVal,
-    //             });
-    //         }
-    //     });
-    // });
-
-    //     // magnificPopup
-    //     $(".portfolio .link").magnificPopup({
-    //         delegate: "a",
-    //         type: "image",
-    //         gallery: {
-    //             enabled: true,
-    //         },
-    //     });
-
-
-
-    // ^ === owl carsouel === /////
+    // ^ === Feedback section using owl-carsouel.js === /////
+    //#region Feedback section using owl-carsouel.js
     $(".owl-carousel").owlCarousel({
         items: 1, // Number of items to display at a time
         loop: true, // Infinite loop
@@ -98,69 +73,39 @@ $(function () {
 
     });
 
-
-    //     // stellar
-    //     wind.stellar();
-
-    // countUp
+    //#endregion
+    // ^ === Numbers section - Counter-up using counterup.js === /////
+    //#region Numbers section - Counter-up using counterup.js
     $(".numbers .counter").countUp({
         delay: 10,
         time: 1500,
     });
+
+    //#endregion
+
+
 });
 
 $(window).on("load", function () {
     // Preloader
-    // $(".loading").addClass("loading-end").fadeOut(1000);
+    $(".loading").addClass("loading-end").fadeOut(5000);
 
-    // // isotope
-    // $(".services .row").isotope({
-    //     // options
-    //     itemSelector: ".col-md-4",
-    // });
-
-    // // isotope
-    $(".gallery").isotope({
+    // ^ === Services section - smooth rearrangement using isotope.js  === /////
+    //#region Services section - smooth rearrangement using isotope.js
+    $("#services .row").isotope({
         // options
-        itemSelector: ".items",
+        itemSelector: ".isoItem",
     });
 
-    var $gallery = $(".gallery").isotope({
-        // options
-    });
+    //#endregion
 
-    // filter items on button click
-    $(".filtering").on("click", "span", function () {
-        var filterValue = $(this).attr("data-filter");
-
-        $gallery.isotope({ filter: filterValue });
-    });
-
-    $(".filtering").on("click", "span", function () {
-        $(this).addClass("active").siblings().removeClass("active");
-    });
-
-
-    // Get the outer height of the current window
-    var windowHeight = window.outerHeight;
-
-    // Display the outer height in the console
-    console.log('Outer Height: ' + windowHeight + ' pixels');
-
-    // Get the inner height of the current window
-    var innerHeight = window.innerHeight;
-
-    // Display the inner height in the console
-    console.log('Inner Height: ' + innerHeight + ' pixels');
-
-
-
-
+    // ^ === Navbar section - Change the Navbar style based on scrolling === /////
+    //#region Change the Navbar style based on scrolling
     window.addEventListener("scroll", function () {
         var scroll = window.scrollY;
         var h_hight = $("#home").outerHeight();
-        console.log("scroll: " + scroll);
-        console.log("h_hight: " + h_hight);
+        // console.log("scroll: " + scroll);
+        // console.log("h_hight: " + h_hight);
 
         if (scroll > h_hight) {
             document.getElementById("navbar").classList.add("nav-scroll");
@@ -169,35 +114,97 @@ $(window).on("load", function () {
 
         }
     });
+    //#endregion
+
 });
 
 
-function filterItems(category) {
-    // get all items
-    var items = document.querySelectorAll('.items');
+// ! NAVBAR SECTION
 
+// ^ === Navbar section - Collapse nav-list when nav-link is clicked using vanilla js === /////
+//#region Navbar section - Collapse nav-list when nav-link is clicked using vanilla js
+function toggleNavbar() {
+    var droplist = document.getElementById('navbarNavDropdown');
+    droplist.classList.toggle('show');
 
-    // Display only the selected category  
-    items.forEach(item => {
-        if (category === 'all' || item.classList.contains(category)) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none';
-        }
-    })
+    if (droplist.classList.contains('show')) {
+        // Add an event listener to close the navbar only when interacting with elements inside it
+        document.addEventListener('click', function (event) {
 
-    // // Perform a smooth transition to rearrange visible items
-    // var gallery = document.getElementById('gallery');
-    // gallery.style.transform = 'translateY(0)';
+            var links = document.querySelectorAll('#navbarNavDropdown .nav-link');
+            // console.log(navbar);
 
-    // // After the transition, reset the transform property to avoid interference with future animations
-    // setTimeout(function () {
-    //     gallery.style.transform = '';
-    // }, 300);
+            links.forEach(element => {
+                var isClickInsideNavbar = element.contains(event.target);
+                console.log(isClickInsideNavbar);
+                if (isClickInsideNavbar === true) {
+                    console.log("trueeeeee");
+                    droplist.classList.remove('show');
+                    // return;
+                }
 
+            });
 
-
+        });
+    };
 };
+//#endregion
+
+// ! HOME SECTION
+
+// ^ === Home section - Image parallax effect using vanilla js === /////
+//#region Home section - Image parallax effect using vanilla js
+document.addEventListener('scroll', function () {
+    let scrollTop = window.scrollY;
+    let speed = 0.3; // Adjust the speed as needed
+
+    document.querySelector('.parallax-section').style.transform = `translateY(${scrollTop * speed}px)`;
+});
+//#endregion
+
+// ^ === Home section - Typing effect using Typed.js === /////
+//#region Home section - Typing effect using Typed.js
+var typed = new Typed('.element', {
+    strings: ['Jerry Daniels', 'Designer', 'Developer', 'Freelancer'],
+    // stringsElement: null,
+    // time before typing starts
+    // startDelay: 1200,
+    // typing speed
+    typeSpeed: 100,
+    // time before backspacing
+    // backDelay: 30,
+    // backspacing speed
+    backSpeed: 50,
+    // loop
+    loop: true,
+    // false = infinite
+    // loopCount: 10,
+    // show cursor
+    // showCursor: true,
+    // character for cursor
+    cursorChar: " |",
+    // shuffle: true,
+    // smartBackspace: false,
+    // attribute to type (null == text)
+    // attr: null,
+    // either html or text
+    // contentType: 'html',
+    // call when done callback function
+    callback: function () { },
+    // starting callback function before each string
+    preStringTyped: function () { },
+    //callback for every typed string
+    onStringTyped: function () { },
+    // callback for reset
+    resetCallback: function () { }
+});
+//#endregion
+
+
+// ! PORTFOLIO SECTION
+
+// ^ === Portfolio section - Links copy event using vanilla js === /////
+//#region Portfolio section - Links copy event using vanilla js
 function copylink1(altText) {
     // Get the image element
 
@@ -384,13 +391,200 @@ function copylink6(altText) {
             console.error('Unable to copy to clipboard', err);
         });
 };
+//#endregion
+
+// ^ === Portfolio section - Popup images using vanilla js === /////
+//#region Portfolio section - Popup images using vanilla js
+//#region pop1
+function openPopup1() {
+    // Add the 'blur' class to the body
+    // document.body.classList.add('blur');
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    // Add the 'no-scroll' class to the body to disable scrolling
+    document.body.classList.add('no-scroll');
+    // Display the popup
+    document.getElementById('popup1').style.display = 'block';
+}
+
+function closePopup1() {
+    // Remove the 'blur' class from the body
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    // Remove the 'no-scroll' class from the body to enable scrolling
+    document.body.classList.remove('no-scroll');
+    // Hide the popup
+    document.getElementById('popup1').style.display = 'none';
+}
+//#endregion
+// ----------------
+//#region pop2
+function openPopup2() {
+    // Add the 'blur' class to the body
+    // document.body.classList.add('blur');
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    // Add the 'no-scroll' class to the body to disable scrolling
+    document.body.classList.add('no-scroll');
+    // Display the popup
+    document.getElementById('popup2').style.display = 'block';
+}
+function closePopup2() {
+    // Remove the 'blur' class from the body
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    // Remove the 'no-scroll' class from the body to enable scrolling
+    document.body.classList.remove('no-scroll');
+    // Hide the popup
+    document.getElementById('popup2').style.display = 'none';
+}
+//#endregion
+// ----------------
+//#region pop3
+function openPopup3() {
+    // Add the 'blur' class to the body
+    // document.body.classList.add('blur');
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    // Add the 'no-scroll' class to the body to disable scrolling
+    document.body.classList.add('no-scroll');
+    // Display the popup
+    document.getElementById('popup3').style.display = 'block';
+}
+function closePopup3() {
+    // Remove the 'blur' class from the body
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    // Remove the 'no-scroll' class from the body to enable scrolling
+    document.body.classList.remove('no-scroll');
+    // Hide the popup
+    document.getElementById('popup3').style.display = 'none';
+}
+//#endregion
+// ----------------
+//#region pop4
+function openPopup4() {
+    // Add the 'blur' class to the body
+    // document.body.classList.add('blur');
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    // Add the 'no-scroll' class to the body to disable scrolling
+    document.body.classList.add('no-scroll');
+    // Display the popup
+    document.getElementById('popup4').style.display = 'block';
+}
+function closePopup4() {
+    // Remove the 'blur' class from the body
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    // Remove the 'no-scroll' class from the body to enable scrolling
+    document.body.classList.remove('no-scroll');
+    // Hide the popup
+    document.getElementById('popup4').style.display = 'none';
+}
+//#endregion
+// ----------------
+//#region pop5
+function openPopup5() {
+    // Add the 'blur' class to the body
+    // document.body.classList.add('blur');
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    // Add the 'no-scroll' class to the body to disable scrolling
+    document.body.classList.add('no-scroll');
+    // Display the popup
+    document.getElementById('popup5').style.display = 'block';
+}
+function closePopup5() {
+    // Remove the 'blur' class from the body
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    // Remove the 'no-scroll' class from the body to enable scrolling
+    document.body.classList.remove('no-scroll');
+    // Hide the popup
+    document.getElementById('popup5').style.display = 'none';
+}
+//#endregion
+// ----------------
+//#region pop6
+function openPopup6() {
+    // Add the 'blur' class to the body
+    // document.body.classList.add('blur');
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    // Add the 'no-scroll' class to the body to disable scrolling
+    document.body.classList.add('no-scroll');
+    // Display the popup
+    document.getElementById('popup6').style.display = 'block';
+}
+function closePopup6() {
+    // Remove the 'blur' class from the body
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    // Remove the 'no-scroll' class from the body to enable scrolling
+    document.body.classList.remove('no-scroll');
+    // Hide the popup
+    document.getElementById('popup6').style.display = 'none';
+}
+//#endregion
+
+//#endregion
+
+// * === Portfolio section - Filtering using #[     vanilla js       ]# === /////
+//#region Portfolio Filtering using vanilla js
 
 
-document.addEventListener('scroll', function () {
-    let scrollTop = window.scrollY;
-    let speed = 0.3; // Adjust the speed as needed
 
-    document.querySelector('.parallax-section').style.transform = `translateY(${scrollTop * speed}px)`;
+// function filterItems(category) {
+//     // get all items
+//     var items = document.querySelectorAll('#gallery .items');
+
+//     // Display only the selected category  
+//     items.forEach(item => {
+//         console.log(item);
+
+//         if (category === 'all' || item.classList.contains(category)) {
+//             item.style.display = 'block';
+//         } else {
+//             item.style.display = 'none';
+//         }
+//     });
+
+//     $(".filtering").on("click", "span", function () {
+//         $(this).addClass("active").siblings().removeClass("active");
+//     });
+// };
+
+
+//#endregion
+
+// * === Portfolio section - Filtering using #[     isotope.js      ]#  === /////
+//#region Portfolio Filtering using isotope.js
+
+$(".gallery").isotope({
+    // options
+    itemSelector: ".items",
 });
 
+var $gallery = $(".gallery").isotope({
+    // options
+});
 
+// filter items on button click
+$(".filtering").on("click", "span", function () {
+    var filterValue = $(this).attr("data-filter");
+
+    $gallery.isotope({ filter: filterValue });
+});
+
+$(".filtering").on("click", "span", function () {
+    $(this).addClass("active").siblings().removeClass("active");
+});
+
+//#endregion
